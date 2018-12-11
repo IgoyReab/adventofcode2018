8b17b1eba34eeb1c999b6ab5c0b6eb441d0f1679
Marco <igoyreab@yogibear.nl>
2018-12-11 19:34:46 +0100

    day-1

---
+++ src/main/java/nl/yogibear/Day_1.java
@@ -0,0 +1,71 @@
+package nl.yogibear;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day_1 {
+
+    static List<String> freqShiftsList = null;
+    static Integer[] freqShifts;
+
+    static long resultingFrequency(long n, int i) {               //recursive function to add all elements together
+        if (i >= freqShifts.length) return n;                    // stop if last element has been added
+        return freqShifts[i] + resultingFrequency(n, i + 1);  //recursive call to this function with the next element
+    }
+
+    static long firstRecuringFrequency() {
+
+        Set<Long> foundFrequencies = new HashSet<Long>();         // set of Longs, if I add an element that already exists it returns false
+
+        long sum = 0;
+        boolean found = false;
+
+        do {
+            for (int i = 0; i < freqShifts.length; i++) {     // loop through the array
+                sum = sum + freqShifts[i];
+                found = !(foundFrequencies.add(sum));       // found becomes true if  an element that already exist is added, if that happens the recuring Frequency has been found
+                if (found) break;
+            }
+        } while (!found);                                     // try agian if not found
+
+        return sum;                                           // return the found value (the one that was double!
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        LocalTime start = LocalTime.now();
+
+        Files.readLines(new File("src/main/resources/day1.txt"), Charset.forName("utf-8"));
+
+        freqShifts = new Integer[freqShiftsList.size()];
+
+        int count = 0;
+
+        for (String s: freqShiftsList) {
+            s =s.replace("+","");
+            freqShifts[count] = Integer.parseInt(s);
+            count ++;
+        }
+
+        System.out.println("Final frequency is " + resultingFrequency(0, 0));
+        LocalTime finish = LocalTime.now();
+        System.out.println("Duration (ms): " + Duration.between(start, finish).toMillis());
+
+        System.out.println("( " + (time2 - time1) + " ms )");
+        
+        start = LocalTime.now();
+        System.out.println("First recuring frequency is " + firstRecuringFrequency());
+        time2 = new Date().getTime();
+        System.out.println("( " + (time2 - time1) + " ms )");
+
+    }
+}
